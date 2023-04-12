c3901de7861076254aa0397c9bb1a14ad5311ac1
Arushi Agarwal <121698199+arushiarushi@users.noreply.github.com>
2023-04-12 11:21:32 -0700

    Add files via upload

---
+++ students.sql
@@ -0,0 +1,21 @@
+CREATE TABLE students(
+	id INTEGER PRIMARY KEY 
+	firstname VARCHAR (40),
+	lastname VARCHAR (80), 
+	age NUMERIC 
+);
+
+INSERT INTO student (id, firstname, lastname, age) 
+VALUES  
+(1, 'Fred', 'Flintstone', 35),
+(2, 'Wilma', 'Flintstone', 29),
+  (3, 'Barney', 'Rubble', 33),
+  (4, 'Betty', 'Rubble', 29),
+  (5, 'Pebbles', 'Flintstone', 1),
+  (6, 'Bam-Bam', 'Rubble', 1),
+   (7, 'Aryan', 'Agarwal', 16),
+  (8, 'Shailesh', 'Agarwal', 47), 
+  (9, 'Arti', 'Jain', 49), 
+  (10, 'Akhilesh', 'Diwedi', 50), 
+  (11, 'Anshul', 'Batish', 21),
+  (12, 'Arushi', 'Agarwal', 19);
