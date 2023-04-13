2c9b5ee141239d14a77bcbcc2e4c3f7dcefe7ef0
Arushi Agarwal <121698199+arushiarushi@users.noreply.github.com>
2023-04-13 11:57:49 -0700

    Update courses.sql

--- courses.sql
+++ courses.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS courses(
 	code VARCHAR (40) NOT NULL PRIMARY KEY,
-	description VARCHAR (400),
 	start DATE NOT NULL,
-	end DATE NOT NULL
+	end DATE NOT NULL,
+	description VARCHAR (400),
 	CHECK (end>start)
 );
 
@@ -13,3 +13,4 @@ INSERT INTO courses(code, start, end, description) VALUES
   ('INFO449A', '2023-09-25', '2023-12-19', 'Introduction to Android'),
   ('BAW0100', '2023-04-01', '2023-06-01', 'Introduction to Basket-Weaving'),
   ('BAW100A', '2023-04-01', '2023-06-01', 'Underwater Basket-Weaving');
+  
